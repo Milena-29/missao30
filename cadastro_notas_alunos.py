3194b6d2a0237f7d2377d085c2e583520fd49193
Milena-29 <milena.lopnascimento@gmail.com>
2026-03-05 11:26:50 -0300

    Update cadastro_notas_alunos.py

--- cadastro_notas_alunos.py
+++ cadastro_notas_alunos.py
@@ -1,29 +1,30 @@
 quantidade_alunos = int(input("Digite o número da quantidade de alunos que serão cadastrados: "))
 
-while True:
+if quantidade_alunos < 10:
+    print("É necessário cadastrar no mínimo 10 alunos! Tente novamente mais tarde.")
+    break
     
-    if quantidade_alunos < 10:
-        print("É necessário cadastrar no mínimo 10 alunos! Tente novamente mais tarde.")
-        break
-        
-    else:
-        alunos = []
+else:
+    alunos = []
 
     for m in range(quantidade_alunos):
         nome = input("Digite o nome do aluno: ")
-        
+
         notas = []
-        
+
         for l in range(3):
-            
-            while nota < 0 or nota > 10:
+
+            while True:
                 nota = float(input("Digite a nota do aluno (0 a 10): "))
-                
+
                 if nota < 0 or nota > 10:
-                    
                     print("Nota inválida! Digite um valor entre 0 e 10.")
+                    continue
+
+                break
+
             notas.append(nota)
-        
+
         media = (notas[0] + notas[1] + notas[2]) / 3
 
         if media >= 7:
@@ -36,7 +37,7 @@ while True:
         alunos.append([nome, notas[0], notas[1], notas[2], media, situacao])
 
     print("\nQuantidade total de alunos cadastrados:", len(alunos))
-    
+
     for aluno in alunos:
         nome = aluno[0]
         nota1 = aluno[1]
@@ -45,8 +46,8 @@ while True:
         media = aluno[4]
         situacao = aluno[5]
 
-        print("Aluno:", nome)
+        print("\nAluno:", nome)
         print("Notas:", nota1, nota2, nota3)
         print("Média:", media)
-
         print("Situação:", situacao)
+
