465506ef139758c68ad501c2598dad8ef68c2593
Milena-29 <milena.lopnascimento@gmail.com>
2026-03-06 10:10:15 -0300

    Update cadastro_notas_alunos.py

--- cadastro_notas_alunos.py
+++ cadastro_notas_alunos.py
@@ -1,23 +1,33 @@
+#Solicitar ao usuário a digitar a quantidade total de alunos
 quantidade_alunos = int(input("Digite o número da quantidade de alunos que serão cadastrados: "))
 
 while True:
 
+    # Verfificação se a quantidade de alunos é menos que 10, caso seja, exibe uma mensagem de erro e encerra o programa.
     if quantidade_alunos < 10:
         print("\nÉ necessário cadastrar no mínimo 10 alunos! Tente novamente mais tarde.")
         break
 
+    else:
+        print("Quantidade válida de alunos! Prosseguindo com o cadastro...")
+
+    # Criaçãon de uma lista para armanezar os dados dos alunos
     alunos = []
 
     for m in range(quantidade_alunos):
 
+        # Pedir ao usuário para que digite o nome do aluno
         nome = input("\nDigite o nome do aluno: ")
 
+
+        # Criação de uma lista para armanezar as notas do aluno
         notas = []
 
         for l in range(3):
 
             while True:
 
+             # Solicitar ao usuário para digitar as três notas do aluno  
                 nota = float(input("Digite a nota do aluno (0 a 10): "))
 
                 if nota < 0 or nota > 10:
@@ -28,8 +38,10 @@ while True:
 
             notas.append(nota)
 
+        # Cálcula da média das notas dos alunos
         media = (notas[0] + notas[1] + notas[2]) / 3
 
+        # Verificação das notas para verificar se o aluno será aprovado,recuperação,reprovado
         if media >= 7:
             situacao = "Aprovado"
 
